06768a61f3c8ff9ee4f152d02b6bfd9a55f68252
Marcel Rotunno <marcel.rotunno@gmail.com>
2026-05-30 23:23:56 -0300

    Refactor code of evaluation to remove elements smaller than a minimum area.

--- avaliacao/evaluator.py
+++ avaliacao/evaluator.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 11 12:45:51 2024
+Created on Fri May 29 18:37:06 2026
 
 @author: Marcel
 """
-# %% Imports general
+
+# %% Import Libraries
 
 from osgeo import gdal
 from pathlib import Path
 import numpy as np
 from tensorflow.keras.models import load_model
 import json
+import warnings
 
 # %% Import from own modules
 
@@ -22,198 +24,140 @@ from ..treinamento.model_trainer import ModelTrainer
 
 from .metric_calculator import RelaxedMetricCalculator
 from .mosaics import MosaicGenerator
-from .utils import stack_uneven
+from .utils import stack_uneven, remove_small_areas
 
-# %% Class definition
+# %%
+
 
 class ModelEvaluator:
-    def __init__(self, x_dir, y_dir, output_dir, label_tiles_dir=None):
+    def __init__(self, x_dir: str, y_dir: str, output_dir: str, label_tiles_dir: str = None):
         self.x_dir = Path(x_dir)
         self.y_dir = Path(y_dir)
         self.output_dir = Path(output_dir)
-        self.label_tiles_dir = Path(label_tiles_dir)
+        self.label_tiles_dir = Path(label_tiles_dir) if label_tiles_dir else None
         
-        self.model = load_model(Path(output_dir) / ModelTrainer.best_model_filename, compile=False, custom_objects={"Patches": Patches, 
-                                                                                                      "MixVisionTransformer": MixVisionTransformer,
-                                                                                                      "SegFormerHead": SegFormerHead,
-                                                                                                      "ResizeLayer": ResizeLayer})
+        # Load Model
+        self.model = load_model(
+            self.output_dir / ModelTrainer.best_model_filename, 
+            compile=False, 
+            custom_objects={
+                "Patches": Patches, 
+                "MixVisionTransformer": MixVisionTransformer,
+                "SegFormerHead": SegFormerHead,
+                "ResizeLayer": ResizeLayer
+            }
+        )
         
-        self._set_numpy_arrays()
-        
-        self.y_mosaics = None
-        if label_tiles_dir:
-            # Load reference mosaics
-            labels_paths = [str(path) for path in self.label_tiles_dir.iterdir() 
-                                 if path.suffix=='.tiff' or path.suffix=='.tif']
-            labels_paths.sort()
-            y_mosaics = [gdal.Open(y_mosaic_path).ReadAsArray() for y_mosaic_path in labels_paths]
-            self.y_mosaics = stack_uneven(y_mosaics)[..., np.newaxis]
-            
-        
-    def _set_numpy_arrays(self):
-        # Load X and Y Train
-        self.x_train = np.load(self.x_dir / 'x_train.npy')
-        self.y_train = np.load(self.y_dir / 'y_train.npy')        
-        
-        # Load X and Y Valid
-        self.x_valid = np.load(self.x_dir / 'x_valid.npy')
-        self.y_valid = np.load(self.y_dir / 'y_valid.npy')
-        
-        # Load X and Y Test
-        self.x_test = np.load(self.x_dir / 'x_test.npy')
-        self.y_test = np.load(self.y_dir / 'y_test.npy')
-        
-    def model_predict(self, x_array, batch_step=2):
-        prob = self.model.predict(x_array, batch_size=batch_step, verbose=1) # Calculate Probabilities
-        
-        pred = np.argmax(prob, axis=-1)[..., np.newaxis] # Calculate Predictions
-        
-        prob = prob[..., 1:2] # Only road probabilites are selected (Class 1)
+        # Mosaics are loaded only if the directory is provided
+        self.y_mosaics = self._load_reference_mosaics() if self.label_tiles_dir else None
 
+    def _load_reference_mosaics(self) -> np.ndarray:
+        labels_paths = sorted([
+            str(p) for p in self.label_tiles_dir.iterdir() 
+            if p.suffix in ('.tiff', '.tif')
+        ])
+        y_mosaics = [gdal.Open(path).ReadAsArray() for path in labels_paths]
+        return stack_uneven(y_mosaics)[..., np.newaxis]
+
+    def model_predict(self, x_array: np.ndarray, batch_step: int = 2) -> tuple:
+        prob = self.model.predict(x_array, batch_size=batch_step, verbose=1)
+        pred = np.argmax(prob, axis=-1)[..., np.newaxis]
+        prob = prob[..., 1:2]  # Only road probabilities (Class 1)
         return prob, pred
-    
-    def _evaluate_train(self, buffers_px, include_avg_precision):
-        # Predict Train
-        if not ( (self.output_dir / 'prob_train.npy').exists() and (self.output_dir / 'pred_train.npy').exists() ):
-            prob_train, pred_train = self.model_predict(x_array=self.x_train)
+
+    def _evaluate_split(self, split_name: str, buffers_px: list, include_avg_precision: bool):
+        """Generic method to evaluate any data split, reducing code duplication."""
+        prob_path = self.output_dir / f'prob_{split_name}.npy'
+        pred_path = self.output_dir / f'pred_{split_name}.npy'
+
+        # Predict and Save if files do not exist
+        if not (prob_path.exists() and pred_path.exists()):
+            # Lazy load data only when needed to save RAM
+            x_array = np.load(self.x_dir / f'x_{split_name}.npy')
+            prob, pred = self.model_predict(x_array=x_array)
+            
+            # Cast to occupy less space
+            np.save(prob_path, prob.astype(np.float16))
+            np.save(pred_path, pred.astype(np.uint8))
+            
+            # Free memory immediately after prediction
+            del x_array 
         
-            # Cast to type to occupy less space para tipos que ocupam menos espaço
-            prob_train = prob_train.astype(np.float16)
-            pred_train = pred_train.astype(np.uint8)
-            
-            np.save(self.output_dir / 'prob_train.npy', prob_train)
-            np.save(self.output_dir / 'pred_train.npy', pred_train)
-            
-        # Load probabilities and prediction
-        if include_avg_precision:
-            prob_train = np.load(self.output_dir / 'prob_train.npy')
-        else:
-            prob_train = None
-            
-        pred_train = np.load(self.output_dir / 'pred_train.npy')
-        
-        # Evaluate for buffer distances
-        for buffer_px in buffers_px:
-            metric_calculator = RelaxedMetricCalculator(y_array=self.y_train, pred_array=pred_train, buffer_px=buffer_px, prob_array=prob_train)
-            metric_calculator.calculate_metrics(include_avg_precision=include_avg_precision)
-            metric_calculator.export_results(output_dir=self.output_dir, group='train') 
-            
-    def _evaluate_valid(self, buffers_px, include_avg_precision):
-        # Predict Valid
-        if not ( (self.output_dir / 'prob_valid.npy').exists() and (self.output_dir / 'pred_valid.npy').exists() ):
-            prob_valid, pred_valid = self.model_predict(x_array=self.x_valid)
-        
-            # Cast to type to occupy less space para tipos que ocupam menos espaço
-            prob_valid = prob_valid.astype(np.float16)
-            pred_valid = pred_valid.astype(np.uint8)
-            
-            np.save(self.output_dir / 'prob_valid.npy', prob_valid)
-            np.save(self.output_dir / 'pred_valid.npy', pred_valid)
-            
-        # Load probabilities and prediction
-        if include_avg_precision:
-            prob_valid = np.load(self.output_dir / 'prob_valid.npy')
-        else:
-            prob_valid = None
-        
-        pred_valid = np.load(self.output_dir / 'pred_valid.npy')
+        # Load targets and predictions
+        y_array = np.load(self.y_dir / f'y_{split_name}.npy')
+        pred_array = np.load(pred_path)
+        prob_array = np.load(prob_path) if include_avg_precision else None
         
         # Evaluate for buffer distances
         for buffer_px in buffers_px:
-            metric_calculator = RelaxedMetricCalculator(y_array=self.y_valid, pred_array=pred_valid, buffer_px=buffer_px, prob_array=prob_valid)
-            metric_calculator.calculate_metrics(include_avg_precision=include_avg_precision)
-            metric_calculator.export_results(output_dir=self.output_dir, group='valid')
+            calculator = RelaxedMetricCalculator(
+                y_array=y_array, 
+                pred_array=pred_array, 
+                buffer_px=buffer_px, 
+                prob_array=prob_array
+            )
+            calculator.calculate_metrics(include_avg_precision=include_avg_precision)
+            calculator.export_results(output_dir=self.output_dir, group=split_name)
+
+    def evaluate_model(self, splits: list = ['valid', 'test'], buffers_px: list = [3], include_avg_precision: bool = False):
+        """Evaluates the model on specified dataset splits."""
+        for split in splits:
+            if split in ['train', 'valid', 'test']:
+                self._evaluate_split(split, buffers_px, include_avg_precision)
+            else:
+                warnings.warn(f"Unknown split '{split}' ignored.")
+
+    def build_test_mosaics(self, prefix: str = 'outmosaic', export_pred_mosaics: bool = False, 
+                           export_prob_mosaics: bool = False, ignore_index: int = 255,
+                           min_area_px=None):
+        if not self.label_tiles_dir:
+            raise ValueError("Cannot build mosaics: label_tiles_dir was not provided.")
             
-    def _evaluate_test(self, buffers_px, include_avg_precision):
-        # Predict Test
-        if not ( (self.output_dir / 'prob_test.npy').exists() and (self.output_dir / 'pred_test.npy').exists() ):
-            prob_test, pred_test = self.model_predict(x_array=self.x_test)
+        print('Building mosaics using pred_test from output directory...')
         
-            # Cast to type to occupy less space para tipos que ocupam menos espaço
-            prob_test = prob_test.astype(np.float16)
-            pred_test = pred_test.astype(np.uint8)
-            
-            np.save(self.output_dir / 'prob_test.npy', prob_test)
-            np.save(self.output_dir / 'pred_test.npy', pred_test)
-            
-        # Load probabilities and prediction
-        if include_avg_precision:
-            prob_test = np.load(self.output_dir / 'prob_test.npy')
-        else:
-            prob_test = None
-        
-        pred_test = np.load(self.output_dir / 'pred_test.npy')
-        
-        # Evaluate for buffer distances
-        for buffer_px in buffers_px:
-            metric_calculator = RelaxedMetricCalculator(y_array=self.y_test, pred_array=pred_test, buffer_px=buffer_px, prob_array=prob_test)
-            metric_calculator.calculate_metrics(include_avg_precision=include_avg_precision)
-            metric_calculator.export_results(output_dir=self.output_dir, group='test')  
-        
-    def evaluate_model(self, evaluate_train=False, evaluate_valid=True, evaluate_test=True, buffers_px=[3],
-                       include_avg_precision=False):
-        # Evaluate train
-        if evaluate_train:
-            self._evaluate_train(buffers_px=buffers_px, include_avg_precision=include_avg_precision)
-        
-        # Evaluate valid
-        if evaluate_valid:
-            self._evaluate_valid(buffers_px=buffers_px, include_avg_precision=include_avg_precision)    
-            
-        # Evaluate test
-        if evaluate_test:
-            self._evaluate_test(buffers_px=buffers_px, include_avg_precision=include_avg_precision) 
-            
-    def build_test_mosaics(self, prefix='outmosaic', export_pred_mosaics=False, 
-                           export_prob_mosaics=False, ignore_index=255):
-        if self.label_tiles_dir is None:
-            raise Exception("Couldn't build mosaics because the label tiles directory was not informed")
-            
-        print('Mosaics will be builded with pred_test saved in output directory')
-        # Load probabilities and prediction
         prob_test = np.load(self.output_dir / 'prob_test.npy')
         pred_test = np.load(self.output_dir / 'pred_test.npy')
         
-        # Load information
         with open(self.y_dir / 'info_tiles_test.json') as fp:   
             info_tiles_test = json.load(fp)
         
-        # See if there are ignored indexes in reference mosaics
-        ignore_in_y_mosaics = ignore_index in self.y_mosaics 
+        ignore_in_y = ignore_index in self.y_mosaics 
         
-        # Build, save and export mosaics for pred
-        mosaics = MosaicGenerator(test_array=pred_test, info_tiles=info_tiles_test, tiles_dir=self.label_tiles_dir,
-                                  output_dir=self.output_dir)
-        mosaics.build_mosaics()
-        if ignore_in_y_mosaics:
-            mosaics.mosaics[self.y_mosaics == ignore_index] = ignore_index            
+        # Helper function to keep DRY
+        def _process_mosaic(test_array, dtype, file_prefix, export_flag, out_prefix):
+            mosaics = MosaicGenerator(test_array=test_array, info_tiles=info_tiles_test, 
+                                      tiles_dir=self.label_tiles_dir, output_dir=self.output_dir)
+            mosaics.build_mosaics(dtype=dtype)
+            if ignore_in_y:
+                mosaics.mosaics[self.y_mosaics == ignore_index] = ignore_index                
+            if min_area_px and file_prefix == 'pred':
+                print(f'Area elements smaller than {min_area_px} px will be removed')
+                mosaics.mosaics = remove_small_areas(mosaics.mosaics, 
+                                                     min_area_px=min_area_px,
+                                                     ignore_index=ignore_index)                
+            mosaics.save_mosaics(prefix=file_prefix)
+            if export_flag:
+                mosaics.export_mosaics(prefix=out_prefix)
+
+        _process_mosaic(pred_test, np.uint8, 'pred', export_pred_mosaics, f"{prefix}_pred_")
+        _process_mosaic(prob_test, np.float32, 'prob', export_prob_mosaics, f"{prefix}_prob_")
+        
+
+
+    def evaluate_mosaics(self, buffers_px: list = [3], include_avg_precision: bool = False, min_area_px: int = None):
+        if self.y_mosaics is None:
+            raise ValueError("Reference mosaics not loaded. Provide label_tiles_dir on initialization.")
             
-        mosaics.save_mosaics(prefix='pred')
-        if export_pred_mosaics:
-            mosaics.export_mosaics(prefix=prefix+'_pred_')
-        
-        # Build, save and export mosaics for prob
-        mosaics = MosaicGenerator(test_array=prob_test, info_tiles=info_tiles_test, tiles_dir=self.label_tiles_dir,
-                                  output_dir=self.output_dir)
-        mosaics.build_mosaics(dtype=np.float32)
-        if ignore_in_y_mosaics:
-            mosaics.mosaics[self.y_mosaics == ignore_index] = ignore_index 
-        mosaics.save_mosaics(prefix='prob')
-        if export_prob_mosaics:
-            mosaics.export_mosaics(prefix=prefix+'_prob_')
-        
-    def evaluate_mosaics(self, buffers_px=[3], include_avg_precision=False,
-                         min_area_px=None):
-        # Load probabilities and prediction for mosaics
-        if include_avg_precision:
-            prob_mosaics = np.load(self.output_dir / 'prob_mosaics.npy')
-        else:
-            prob_mosaics = None
-            
+        prob_mosaics = np.load(self.output_dir / 'prob_mosaics.npy') if include_avg_precision else None
         pred_mosaics = np.load(self.output_dir / 'pred_mosaics.npy')
         
-        # Evaluate for buffer distances
         for buffer_px in buffers_px:
-            metric_calculator = RelaxedMetricCalculator(y_array=self.y_mosaics, pred_array=pred_mosaics, buffer_px=buffer_px, prob_array=prob_mosaics, min_area_px=min_area_px)
-            metric_calculator.calculate_metrics(include_avg_precision=include_avg_precision)
-            metric_calculator.export_results(output_dir=self.output_dir, group='mosaics')  
+            calculator = RelaxedMetricCalculator(
+                y_array=self.y_mosaics, 
+                pred_array=pred_mosaics, 
+                buffer_px=buffer_px, 
+                prob_array=prob_mosaics, 
+                min_area_px=min_area_px
+            )
+            calculator.calculate_metrics(include_avg_precision=include_avg_precision)
+            calculator.export_results(output_dir=self.output_dir, group='mosaics')
--- avaliacao/utils.py
+++ avaliacao/utils.py
@@ -10,6 +10,7 @@ Created on Tue Jan  7 15:40:23 2025
 # %% Imports
 
 import numpy as np
+from skimage import morphology
 
 # %% Function for "one-hot codification" of binary probability
 
@@ -52,3 +53,64 @@ def stack_uneven(arrays, fill_value=0):
       # Overwrite a block slice of `result` with this array `a`
       result[i][slices] = a
     return result
+
+# %% Function to remove small areas from an array
+
+def remove_small_areas(array: np.ndarray, min_area_px: int, ignore_index: int=255) -> np.ndarray:
+    if len(array.shape) != 4 or array.shape[-1] != 1:
+        raise ValueError('Array shape must be in [B, H, W, 1] format.')
+    
+    # Mask valid pixels
+    masked_array = array * (array != ignore_index)
+    
+    cleaned_array = np.zeros_like(array)
+    
+    # Removes areas with a loop
+    for b in range(array.shape[0]):
+        img_slice = masked_array[b, :, :, 0]
+        
+        cleaned_slice = morphology.area_opening(img_slice, min_area_px, connectivity=1)
+        
+        cleaned_array[b, :, :, 0] = cleaned_slice
+        
+    # Return ignore index to array
+    cleaned_array[~(array != ignore_index)] = ignore_index
+        
+    return cleaned_array 
+
+# %% Function to remove small areas from an array
+
+def remove_small_areas_generic(
+    array: np.ndarray, 
+    min_area_px: int, 
+    ignore_index: int = 255, 
+    background_index: int = 0
+) -> np.ndarray:
+    if len(array.shape) != 4 or array.shape[-1] != 1:
+        raise ValueError('Array shape must be in [B, H, W, 1] format.')
+    
+    # Start with a clean copy of the original array.
+    # This ensures ignore_index and all valid classes are preserved by default.
+    cleaned_array = array.copy()
+    
+    # Find all unique classes that are NOT the background or the ignore index
+    unique_classes = np.unique(array)
+    unique_classes = unique_classes[(unique_classes != ignore_index) & (unique_classes != background_index)]
+    
+    for b in range(array.shape[0]):
+        img_slice = array[b, :, :, 0]
+        
+        for cls in unique_classes:
+            # 1. Isolate ONLY the pixels belonging to the current class
+            class_mask = (img_slice == cls)
+            
+            # 2. Find connected components of this class and remove small ones
+            cleaned_mask = morphology.area_opening(class_mask, min_area_px, connectivity=1)
+            
+            # 3. Identify exactly which pixels were removed
+            removed_pixels = class_mask & ~cleaned_mask
+            
+            # 4. Safely turn ONLY those removed pixels into the designated background
+            cleaned_array[b, :, :, 0][removed_pixels] = background_index
+            
+    return cleaned_array
