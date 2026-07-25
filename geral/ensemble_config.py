84ca50daa4082c63c955ef782bc71a1e057223ee
Marcel Rotunno <marcel.rotunno@gmail.com>
2026-07-25 15:54:59 -0300

    Delete import not used and training loop only for U-CE

--- geral/ensemble_config.py
+++ geral/ensemble_config.py
@@ -11,16 +11,11 @@ from ..entropy.utils import UncertaintyMetric, DataGroups
 from .utils import merge_dicts_with_preference
 
 from dataclasses import dataclass, field
-from typing import Optional, Callable
 from pathlib import Path
 import yaml
 # 1. Define your Loss Registry mapping strings to actual functions
 # Import your custom losses here
 from package_doc.treinamento import losses
-from package_doc.treinamento.custom_loss import masked_cce, custom_offset_entropy_loss
-from package_doc.treinamento.losses2 import dice_loss, combo_loss, weighted_cce
-
-from tensorflow.keras.losses import CategoricalCrossentropy
 
 # %%
 
